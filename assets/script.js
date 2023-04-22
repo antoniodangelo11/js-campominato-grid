e30999bcc00b54a38a459b81b1604ab433454ae5
antoninodangelo11 <reapercsp@gmail.com>
2023-04-22 21:01:26 +0200

    add bonus

--- assets/script.js
+++ assets/script.js
@@ -7,33 +7,50 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 
 /* ESECUZIONE DEL PROGRAMMA */
 
-/* EVENTLISTENER A TUTTI GLI ELEMENTI DELLA GRIGLIA */
+/* DEFINIZIONI DELLE VARIBILI */
 
-const eleGrid = document.querySelector('.grid');
 const eleBtn = document.querySelector('.btn_play');
-const eleDiff = document.querySelector('#difficult').value;
-
-eleBtn.addEventListener("click", function () {
-	createGrid(100, eleGrid);
-});
+const listCells = document.querySelectorAll('.cell');
+	
+for (let i = 0; i < listCells.length; i++) {
+  const cell = listCells[i];
+  cell.addEventListener("click", function(){
+	this.classList.toggle("clicked");
+  });
+}
 
 
 /* DEFINIZIONI DELLE FUNZIONI */
 
+/* FUNZIONE DI ATTIVAZIONE DEL PROGRAMMA */
+eleBtn.addEventListener("click", function () {
+    // debugger
+    const eleGrid = document.querySelector('.grid');
+    const eleDifficulty = document.getElementById("difficulty");
+    let value = eleDifficulty.options[eleDifficulty.selectedIndex].value;
+    console.log(value);
+    
+    
+    if (value == "100") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add("grid_easy");
+        createGrid(100, eleGrid);
+    } else if (value == "81") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add( "grid_medium");
+        createGrid(81, eleGrid);
+    } else if (value == "49") {
+        eleGrid.classList.remove("grid_easy", "grid_medium", "grid_hard");
+        eleGrid.classList.add("grid_hard");
+        createGrid(49, eleGrid);
+    }
+});
+
+
+/* FUNZIONE DI CREAZIONE DELLE GRIGLIE */
 function createGrid(numCells, eleGrid) {
-	
 	eleGrid.innerHTML = '';
-	
-	for (let i = 1; i < numCells + 1; i++) {
+		for (let i = 1; i < numCells + 1; i++) {
 		eleGrid.innerHTML += `<div class="cell">${i}</div>`;
 	}
-	
-	const listCells = document.querySelectorAll('.cell');
-	
-	for (let i = 0; i < listCells.length; i++) {
-	  const cell = listCells[i];
-	  cell.addEventListener("click", function(){
-		this.classList.toggle("clicked");
-	  });
-  	}
 }
